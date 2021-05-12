7754b8d535f9a9c31f31a65c1ff0eac0c63c78d3
Amy Hua <aymay98@gmail.com>
2021-05-12 09:53:24 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+
+        while(true) {
+            System.out.print("What is your move? Enter rock, paper, or scissors. To quit the game, enter quit. ");
+            String myMove = in.nextLine();
+
+            if (myMove.equals("quit")) {
+                break;
+            }
+            if (!myMove.equals("rock") && !myMove.equals("paper") && !myMove.equals("scissors")) {
+                System.out.println("Your move isn't valid! Try again");
+            } else {
+                int rand = (int) (Math.random() * 3);
+                String opponentMove = "";
+                if (rand == 0) {
+                    opponentMove = "rock";
+                } else if (rand == 1) {
+                    opponentMove = "paper";
+                } else {
+                    opponentMove = "scissors";
+                }
+                System.out.println("Opponent move: " + opponentMove);
+
+                if (myMove.equals(opponentMove)) {
+                    System.out.println("It's a tie!");
+                } else if ((myMove.equals("rock") && opponentMove.equals("scissors"))
+                        || (myMove.equals("scissors") && opponentMove.equals("paper"))
+                        || (myMove.equals("paper") && opponentMove.equals("rock"))) {
+                    System.out.println("You won!");
+                } else {
+                    System.out.println("You lost!");
+                }
+
+            }
+
+        }
+        System.out.println("Thanks for playing Rock, Paper, Scissors!");
+
+    }
+}
